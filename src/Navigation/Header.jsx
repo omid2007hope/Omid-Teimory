20324eecd99d35687ce6a93e956942e05706241b
omid2007hope <omid85hope@gmail.com>
2025-11-13 00:11:46 +0100

    Home Page Foundation

--- src/Navigation/Header.jsx
+++ src/Navigation/Header.jsx
@@ -2,29 +2,35 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="fixed z-50 w-screen h-20 flex flex-row justify-between items-center bg-blue-950 text-blue-50 border-b-2 border-blue-50">
-      <div className="w-1/2 flex flex-row justify-center items-center"></div>
-      <div className="w-1/2 flex flex-row justify-between items-center">
+    <header className="fixed z-50 w-screen h-17 flex flex-row justify-between items-center bg-blue-950 text-blue-50 border-b-2 border-blue-50">
+      <div className="w-1/2 h-full flex flex-row justify-start items-center">
+        <img
+          src="https://tse3.mm.bing.net/th/id/OIP.QdIrCNdF2ZVylABjjes1LAHaGq?cb=ucfimgc2&rs=1&pid=ImgDetMain&o=7&rm=3"
+          alt=""
+          className="w-10 ml-5 opacity-90 rounded-md flex flex-row justify-center items-center border"
+        />
+      </div>
+      <div className="w-full md:w-1/2 lg:w-1/2 h-full flex flex-row justify-evenly md:justify-evenly lg:justify-end items-center">
         <Link
-          className="md:py-3 lg:py-3 md:px-25 lg:px-25 hover:font-bold transition-normal"
+          className="w-19.5 md:w-full lg:w-40 h-full flex flex-row justify-center items-center hover:font-bold transition-normal"
           to="/"
         >
           Home
         </Link>
         <Link
-          className="md:py-3 lg:py-3 md:px-25 lg:px-25 hover:font-bold transition-normal"
+          className="w-19.5 md:w-full lg:w-40 h-full flex flex-row justify-center items-center hover:font-bold transition-normal"
           to="/About"
         >
           About
         </Link>
         <Link
-          className="md:py-3 lg:py-3 md:px-25 lg:px-25 hover:font-bold transition-normal"
+          className="w-19.5 md:w-full lg:w-40 h-full flex flex-row justify-center items-center hover:font-bold transition-normal"
           to="/Contact"
         >
           Contact
         </Link>
         <Link
-          className="md:py-3 lg:py-3 md:px-25 lg:px-25 hover:font-bold transition-normal"
+          className="w-19.5 md:w-full lg:w-40 h-full flex flex-row justify-center items-center hover:font-bold transition-normal"
           to="/Project"
         >
           Project
--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,7 +1,26 @@
 function Home() {
   return (
-    <div className="w-screen h-screen flex flex-row justify-center items-center bg-blue-950 text-blue-50">
-      Home
+    <div className="w-screen h-screen flex flex-col md:flex-row lg:flex-row justify-between items-center bg-blue-900 text-blue-50">
+      <div className="h-1/2 w-full md:h-full lg:h-full md:w-1/2 lg:w-1/2 flex flex-col justify-center items-center ">
+        <div className="h-full w-full flex flex-row justify-start items-center">
+          <img
+            src="https://tse4.mm.bing.net/th/id/OIP.5Qp2V7WQayY9pl6ZcFq_ZgHaHa?cb=ucfimgc2&rs=1&pid=ImgDetMain&o=7&rm=3"
+            alt=""
+            className=" w-55 md:w-90 lg:w-120 z-100 mt-15 md:mt-0 lg:mt-0 ml-20 md:ml-15 lg:ml-30 rounded-2xl opacity-100 border border-blue-950"
+          />
+          <img
+            src="https://tse4.mm.bing.net/th/id/OIP.5Qp2V7WQayY9pl6ZcFq_ZgHaHa?cb=ucfimgc2&rs=1&pid=ImgDetMain&o=7&rm=3"
+            alt=""
+            className="w-55 md:w-90 lg:w-120 fixed top-22 md:top-95 lg:top-80 left-20 md:left-15 lg:left-30 z-75 opacity-75  rounded-2xl border border-blue-950 rotate-8"
+          />
+          <img
+            src="https://tse4.mm.bing.net/th/id/OIP.5Qp2V7WQayY9pl6ZcFq_ZgHaHa?cb=ucfimgc2&rs=1&pid=ImgDetMain&o=7&rm=3"
+            alt=""
+            className="w-55 md:w-90 lg:w-120 fixed top-22 md:top-95 lg:top-80 left-20 md:left-15 lg:left-30 z-50 opacity-50 rounded-2xl border border-blue-950 rotate-353"
+          />
+        </div>
+      </div>
+      <div className="h-1/2 w-full md:h-full lg:h-full md:w-1/2 lg:w-1/2 border"></div>
     </div>
   );
 }
